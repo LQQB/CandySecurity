4152f1e96879d5618dd224a579d48cba616ad420
LQQB <LQQB.com>
2018-08-28 16:11:00 +0800

    添加记住我功能，properties资源文件替换成yml

--- candy-security-browser/src/main/java/com/candy/security/browser/BrowerSecurityConfig.java
+++ candy-security-browser/src/main/java/com/candy/security/browser/BrowerSecurityConfig.java
@@ -7,12 +7,16 @@ import com.candy.security.core.authentication.mobile.SmsCodeAuthenticationSecuri
 import com.candy.security.core.properties.SecurityConstants;
 import com.candy.security.core.properties.SecurityProperties;
 import com.candy.security.core.validate.code.ValidateCodeSecurityConfig;
+import org.apache.tomcat.jdbc.pool.DataSource;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.authentication.rememberme.JdbcTokenRepositoryImpl;
+import org.springframework.security.web.authentication.rememberme.PersistentTokenRepository;
 
 /**
  * @author LiQB
@@ -34,11 +38,11 @@ public class BrowerSecurityConfig extends AbstractChannelSecurityConfig {
     @Autowired
     private CandyAuthenticationFailureHanler candyAuthenticationFailureHanler;
 
-//    @Autowired
-//    private DataSource dataSource;
+    @Autowired
+    private DataSource dataSource;
 
-//    @Autowired
-//    private UserDetailsService userDetailsService;
+    @Autowired
+    private UserDetailsService userDetailsService;
 
     @Autowired
     private ValidateCodeSecurityConfig validateCodeSecurityConfig;
@@ -51,38 +55,28 @@ public class BrowerSecurityConfig extends AbstractChannelSecurityConfig {
     @Autowired
     private SmsCodeAuthenticationSecurityConfig smsCodeAuthenticationSecurityConfig;
 
-//    @Bean
-//    public PersistentTokenRepository persistentTokenRepository() {
-//        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
-//        tokenRepository.setDataSource(dataSource);
+    @Bean
+    public PersistentTokenRepository persistentTokenRepository() {
+        JdbcTokenRepositoryImpl tokenRepository = new JdbcTokenRepositoryImpl();
+        tokenRepository.setDataSource(dataSource);
 //        tokenRepository.setCreateTableOnStartup(true);
-//        return tokenRepository;
-//    }
+        return tokenRepository;
+    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         //这是密码登录相关的配置
         applyPasswordAuthenticationConfig(http);
 
-//        ValidateCodeFilter validateCodeFilter = new ValidateCodeFilter();
-//        validateCodeFilter.setAuthenticationFailureHandler(candyAuthenticationFailureHanler);
-//        validateCodeFilter.setSecurityProperties(securityProperties);
-//        validateCodeFilter.afterPropertiesSet();
-//
-//        SmsCodeFilter smsCodeFilter = new SmsCodeFilter();
-//        smsCodeFilter.setAuthenticationFailureHandler(candyAuthenticationFailureHanler);
-//        smsCodeFilter.setSecurityProperties(securityProperties);
-//        smsCodeFilter.afterPropertiesSet();
-
         http.apply(validateCodeSecurityConfig)
                 .and()
                 //短信验证相关的配置
                 .apply(smsCodeAuthenticationSecurityConfig)
-//                .and()
-//                .rememberMe()
-//                    .tokenRepository(persistentTokenRepository())
-//                    .tokenValiditySeconds(securityProperties.getBrowser().getRememberMeSeconds())
-//                    .userDetailsService(userDetailsService)
+                .and()
+                .rememberMe()
+                    .tokenRepository(persistentTokenRepository())
+                    .tokenValiditySeconds(securityProperties.getBrowser().getRememberMeSeconds())
+                    .userDetailsService(userDetailsService)
                 .and()
                 .authorizeRequests()    // 定义哪些URL需要被保护、哪些不需要被保护
                 .antMatchers(
